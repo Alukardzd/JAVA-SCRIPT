894b8dc796d5b7c18c2f5baad89adccdda6c4514
Guilherme <alukrd02@hotmail.com>
2024-08-22 22:02:47 -0300

    atts

---
+++ Curso Com Guanabara/ex015/ambiente.js
@@ -0,0 +1,11 @@
+let num = [
+    5, 8 ,9
+]
+num[3] = 6
+num.push(1)
+num.sort()
+//console.log(num.length)
+//console.log(num)
+for(let pos = 0; pos < num.length; pos++) {
+    console.log(num[pos])
+}
