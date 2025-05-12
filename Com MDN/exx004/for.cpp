ccb9b07f5bda0244c6f4e02d4b9fc9a16b9e26ed
Guilherme <alukrd02@hotmail.com>
2025-05-12 20:32:57 -0300

    estudos c++

---
+++ Com MDN/exx004/for.cpp
@@ -0,0 +1,16 @@
+// static1.cpp
+// compile with: /EHsc
+#include <iostream>
+
+using namespace std;
+void showstat( int curr ) {
+   static int nStatic;    // Value of nStatic is retained
+                          // between each function call
+   nStatic += curr;
+   cout << "nStatic is " << nStatic << endl;
+}
+
+int main() {
+   for ( int i = 0; i < 5; i++ )
+      showstat( i );
+}
---
+++ Com MDN/exx004/hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main(int argc, char** argv){
+
+    std::cout << "Olá, Mundo";
+    system("pause");
+    return 0;
+}
